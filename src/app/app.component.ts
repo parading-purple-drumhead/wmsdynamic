e57891bf66877cb89876e1c27b1a3dee6c9c4d68
varunkondreddy <45779365+varunkondreddy@users.noreply.github.com>
2020-02-09 12:09:00 +0530

    back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
-import { Platform,NavController } from '@ionic/angular';
+import { Component, ViewChildren, QueryList, OnDestroy } from '@angular/core';
+import { Platform,NavController, IonRouterOutlet, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
+import { Plugins } from '@capacitor/core';
+const { Toast } = Plugins;
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+  backButtonSubscription;
+  @ViewChildren(IonRouterOutlet) routerOutlets: QueryList<IonRouterOutlet>;
+  lastTimeBackPress = 0;
+  timePeriodToExit = 2000;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    public alertController: AlertController,
+    private statusBar: StatusBar,
+    public router: Router,
   ) {
     this.initializeApp();
   }
@@ -20,7 +29,67 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+      this.backButtonEvent();
       this.splashScreen.hide();
     });
+  }
+
+  backButtonEvent() {
+    this.backButtonSubscription = this.platform.backButton.subscribe(async () => {
+      this.routerOutlets.forEach((outlet: IonRouterOutlet) => {
+        if (outlet && outlet.canGoBack()) {
+          outlet.pop();
+        } else if (this.router.url === "/home") {
+          // this.presentAlertConfirm();
+          if (new Date().getTime() - this.lastTimeBackPress < this.timePeriodToExit) {
+            navigator['app'].exitApp();
+          } else {
+            this.showToast('Press back again to exit App.');
+            this.lastTimeBackPress = new Date().getTime();
+          }
+        }
+      });
+    });
+  }
+  async showToast(msg) {
+    await Toast.show({
+      text: msg
+    });
+  }
+
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Confirm to Exit App !!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Exit',
+          handler: () => {
+            console.log('Confirm Okay');
+            navigator["app"].exitApp();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+
+
+
+
+
+  //Called when view is left
+  ngOnDestroy() {
+    // Unregister the custom back button action for this page
+    this.backButtonSubscription.unsubscribe();
   }
 }
